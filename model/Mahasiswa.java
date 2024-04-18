dc5b53b02210bac9f9a88696ca398f4a6ee61e2e
Febrian Cahyo Romadhon <otacool121@gmail.com>
2024-04-18 15:22:30 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ==============");
+  System.out.println("NPM : 2210010032");
+  System.out.println("Nama : Febrian Cahyo Romadhon");
+  System.out.println("========================");
+ }
+}
